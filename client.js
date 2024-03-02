e953f943f37feb9ad66e0b83e91b7bc1713f454a
Abdel Hak <abdelhak@Abdels-MacBook-Pro.local>
2024-03-02 18:18:32 +0100

    first

---
+++ client.js
@@ -0,0 +1,29 @@
+const net = require("net");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const client = new net.Socket();
+
+// Connect to the server
+client.connect(3000, "192.168.1.69", () => {
+  console.log("Connected to server");
+});
+
+// Listen for data from the server
+client.on("data", (data) => {
+  console.log(`Received: ${data}`);
+});
+
+// Read user input and send it to the server
+rl.on("line", (input) => {
+  client.write(input);
+});
+
+// Handle client disconnection
+client.on("close", () => {
+  console.log("Connection closed");
+});
